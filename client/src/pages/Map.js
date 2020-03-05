97acd3f27c0736da34a42e9ddf3defe3253e2a42
Narek <gurjiev42@gmail.com>
2020-03-05 21:13:25 +0200

    Fix styles bugs in Map. Add autolocation by coordinates.

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import OLMap1 from '../components/map/openlayers/OLMap'
+import OpenlayersMap from '../components/openlayers-map'
 
 
 class Map extends Component {
     render() {
         return (
-            <h1><OLMap1/></h1>
+            <OpenlayersMap/>
         );
     }
 }
