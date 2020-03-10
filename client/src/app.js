cc14761c80af4a9e891462f81fd7e3d6d5a80bfe
Vadim <fun.rovno@gmail.com>
2020-03-10 17:57:27 +0200

    fix

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD:client/src/app.js
 import NavigationBar from './components/navigation-bar';
 import Home from './pages/home';
 import About from './pages/about';
@@ -11,20 +10,7 @@ import Registartion from './pages/registartion';
 import Map from './pages/map';
 import NoMatch from './pages/no-match';
 import Restaurant from './components/restaurant-item';
-=======
-import NavigationBar from './components/NavigationBar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import ListRestaurant from './pages/ListRestaurant';
-import Login from './pages/Login';
-import Registartion from './pages/Registartion';
-import Map from './pages/Map';
-import NoMatch from './pages/NoMatch';
-
 import Feedback from './components/feedback'
-
->>>>>>> feature_9_feedbacks_rybacuk:client/src/App.js
 
 class App extends Component {
   render() {
@@ -55,15 +41,11 @@ class App extends Component {
           <Route path="/contact">
             <Contact />
           </Route>
-<<<<<<< HEAD:client/src/app.js
-          <Route>
-=======
           <Route >
             {/* Temporarily  in feedback we put rest. id*/}
             <Feedback /> 
           </Route>
           <Route >
->>>>>>> feature_9_feedbacks_rybacuk:client/src/App.js
             <NoMatch />
           </Route>
         </Switch>
