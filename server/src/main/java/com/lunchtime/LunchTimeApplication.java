f2c1c9b448eb86781dacdad17a6cb10b677af956
Andriy_Melnyk <Andriy24681995@gmail.com>
2020-03-10 17:41:20 +0200

    first demo code not workin have a few errors

--- server/src/main/java/com/lunchtime/LunchTimeApplication.java
+++ server/src/main/java/com/lunchtime/LunchTimeApplication.java
@@ -3,9 +3,6 @@ package com.lunchtime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
-
 @SpringBootApplication
 
 public class LunchTimeApplication {
