1a4151f1c3690d28ddd8c9de5e42e42f3c5b6823
Vadim <fun.rovno@gmail.com>
2020-03-10 14:26:40 +0200

    first configuration to feedback(back), add data.sql

---
+++ server/src/main/java/com/lunchtime/service/FeedbackService.java
@@ -0,0 +1,22 @@
+package com.lunchtime.service;
+
+import com.lunchtime.models.Feedback;
+import com.lunchtime.repository.FeedbackRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
+public class FeedbackService {
+  
+  private final FeedbackRepository feedbackRepository;
+
+  public FeedbackService(FeedbackRepository feedbackRepository) {
+    this.feedbackRepository = feedbackRepository;
+  }
+
+  public List<Feedback> findByRestIdOrderByDateDesc(Long id){
+    return feedbackRepository.findByRestIdOrderByDateDesc(id);
+  }
+
+}
