efa4c88833eb00022e103d589a912942e62e2c75
Vadim <fun.rovno@gmail.com>
2020-03-11 10:07:13 +0200

    fix model feedback add lombok

--- server/src/main/java/com/lunchtime/models/Feedback.java
+++ server/src/main/java/com/lunchtime/models/Feedback.java
@@ -1,5 +1,8 @@
 package com.lunchtime.models;
 
+import lombok.Setter;
+import lombok.Getter;
+
 import javax.persistence.*;
 import java.sql.Date;
 
@@ -8,80 +11,31 @@ public class Feedback {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @Getter @Setter
   private Long id;
 
   @Column(name="description", columnDefinition="TEXT")
+  @Getter @Setter
   private String description;
+
+  @Getter @Setter
   private Boolean isActive;
+
+  @Getter @Setter
   private Date date;
+
+  @Getter @Setter
   private String userName;
+
+  @Getter @Setter
   private Long restId;
+
+  @Getter @Setter
   private Integer counterLike;
+
+  @Getter @Setter
   private Integer counterDislike;
 
-  public Boolean getActive() {
-    return isActive;
-  }
-
-  public void setActive(Boolean active) {
-    isActive = active;
-  }
-
-  public String getUserName() {
-    return userName;
-  }
-
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
-
-  public Long getRestId() {
-    return restId;
-  }
-
-  public void setRestId(Long restId) {
-    this.restId = restId;
-  }
-
-  public Integer getCounterLike() {
-    return counterLike;
-  }
-
-  public void setCounterLike(Integer counterLike) {
-    this.counterLike = counterLike;
-  }
-
-  public Integer getCounterDislike() {
-    return counterDislike;
-  }
-
-  public void setCounterDislike(Integer counterDislike) {
-    this.counterDislike = counterDislike;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public Date getDate() {
-    return date;
-  }
-
-  public void setDate(Date date) {
-    this.date = date;
-  }
 
   Feedback(){ }
 
