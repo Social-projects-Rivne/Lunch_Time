e99db5d43782ff01a103d7e4ccbb2819199bb811
narekacy <gurjiev42@gmail.com>
2020-03-17 14:05:37 +0200

    Add 'Event filter bar' component

---
+++ client/src/components/shared/event-filter-bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchFilterToolbar from './search-filter-toolbar';
+import eventData from '../data/event-data';
+
+class EventFilterBar extends React.Component {
+  render() {
+    // const bool {is date} ?
+    return (
+      <SearchFilterToolbar data={eventData} />
+    );
+  }
+}
+
+export default EventFilterBar;
