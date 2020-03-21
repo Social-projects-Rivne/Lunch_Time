3de4a6adf327fee59cb965a47dbce2d0e07ec091
narekacy <gurjiev42@gmail.com>
2020-03-21 15:57:40 +0200

    Modified 'OpenLayers' component
    Now we can pass latitude and langitute props here

---
+++ client/src/components/info/map.js
@@ -0,0 +1,2 @@
+export const latitude = 40.172459;
+export const longitude = 44.511905;
--- client/src/pages/map.js
+++ client/src/pages/map.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import OpenlayersMap from '../components/shared/openlayers-map';
-
+import { latitude, longitude } from '../components/info/map';
 
 class Map extends Component {
   render() {
     return (
-      <OpenlayersMap />
+      <OpenlayersMap latitude={latitude} longitude={longitude} />
     );
   }
 }
