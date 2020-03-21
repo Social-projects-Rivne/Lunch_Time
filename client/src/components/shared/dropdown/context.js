3a78485eb2a07193e7ae3bccff7188884bcfe079
narekacy <gurjiev42@gmail.com>
2020-03-21 16:07:11 +0200

    Modified search context logic

--- client/src/components/shared/dropdown/context.js
+++ client/src/components/shared/dropdown/context.js
@@ -5,17 +5,20 @@ import View from './view';
 class Context extends Component {
   render() {
     const { info } = this.props;
-    return (
-      info && info.length > 0 && info.map((e) => {
-        return (
-          <View
-            id={e.id}
-            name={e.name}
-            values={e.values}
-            key={e.id}
-          />
-        );
-      }));
+    if (info.length > 0) {
+      return (
+        info.map((e) => {
+          return (
+            <View
+              id={e.id}
+              name={e.name}
+              values={e.values}
+              key={e.id}
+            />
+          );
+        }));
+    }
+    return null;
   }
 }
 
