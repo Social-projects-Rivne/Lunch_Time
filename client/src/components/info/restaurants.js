a1824a52c9cd495899554f9bb3757e30baa6cc81
Voz4Nick <vmm408@gmail.com>
2020-03-21 23:27:42 +0200

    edit restaurant info file, add more filters

--- client/src/components/info/restaurants.js
+++ client/src/components/info/restaurants.js
@@ -1,26 +1,44 @@
+const sortBy = ['Relevance', 'Distance', 'Your match'];
 const city = ['Вінницька область', 'Волинська область', 'Дніпропетровська область', 'Донецька область',
   'Житомирська область', 'Закарпатська область', 'Запорізька область', 'Івано-Франківська область',
   'Київська область', 'Кіровоградська область', 'Луганська область', 'Львівська область', 'Миколаївська область',
   'Одеська область', 'Полтавська область', 'Рівненська область', 'Сумська область', 'Тернопільська область',
   'Харківська область', 'Херсонська область', 'Хмельницька область', 'Черкаська область', 'Чернівецька область',
   'Чернігівська область'];
-const rating = ['5 star', '4 star', '3 star', '2 star', '1 star'];
-const types = ['cafe', 'bar', 'restaurant'];
+const rating = ['Any', '3,5 star', '4 star', '4,5 star'];
+const hours = ['Any', 'Open now', 'Custom'];
+const yourPastVisits = ['Any', 'Visited', 'Haven`t visited'];
+const types = ['Restaurants', 'Cafes', 'Bars'];
 
 const info = [
   {
-    id: 'get-location-city',
-    name: 'Location',
+    id: 'sort-by',
+    name: 'Sort by',
+    values: sortBy,
+  },
+  {
+    id: 'choose-city',
+    name: 'City',
     values: city,
   },
   {
-    id: 'sort-by-rating',
-    name: 'By rating',
+    id: 'choose-rating',
+    name: 'Rating',
     values: rating,
   },
   {
-    id: 'sort-by-types',
-    name: 'By types',
+    id: 'choose-hours',
+    name: 'Hours',
+    values: hours,
+  },
+  {
+    id: 'choose-visits',
+    name: 'Your past visits',
+    values: yourPastVisits,
+  },
+  {
+    id: 'choose-type',
+    name: 'Food & drinks',
     values: types,
   },
 ];
