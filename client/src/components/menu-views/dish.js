6def4e6ded31e4f385e9291890e6193b82a75835
Andrii Nazaruk <andriy_nazaruk@yahoo.de>
2020-03-21 15:20:27 -0700

    added functionality to menu view

--- client/src/components/menu-views/dish.js
+++ client/src/components/menu-views/dish.js
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import Api from '../../services/api';
 
 
 class Dish extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categoryfood: [],
+    };
+  }
+
+  componentDidMount() {
+    const { dish, isFetching } = this.props;
+    if (isFetching) {
+      this.getCategoryFood(dish.categoryfoodId);
+    }
+  }
+
+  getCategoryFood(id) {
+    Api.getOne('category', id)
+      .then((response) => {
+        if (response.error) {
+          // eslint-disable-next-line no-console
+          console.error(response);
+          return;
+        }
+        this.setState({
+          categoryfood: response.data,
+        });
+      });
+  }
+
   render() {
+    const { dish } = this.props;
+    const { categoryfood } = this.state;
     return (
       <Container>
-        Here must be dish name
+        <Container className="category-food">
+          <h3>
+            <span>{categoryfood.name}</span>
+          </h3>
+        </Container>
+        <Container className="dish">
+          <Row>
+            <span>{dish.name}</span>
+            <span>{ dish.ingredients}</span>
+          </Row>
+        </Container>
       </Container>
     );
   }
 }
+
+Dish.propTypes = {
+  dish: PropTypes.shape([]).isRequired,
+  isFetching: PropTypes.bool.isRequired,
+};
+
 
 export default Dish;
--- client/src/components/menu-views/menu-view.js
+++ client/src/components/menu-views/menu-view.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import {
+  Container, Row, Spinner, ButtonToolbar,
+} from 'react-bootstrap';
 import Header from './menu-header';
 import Dish from './dish';
+import Api from '../../services/api';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuitemdishes: [],
+      isFetching: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getAll();
+  }
+
+  getAll() {
+    Api.getAll('menuitemdish')
+      .then((response) => {
+        if (response.error) {
+          console.error(response);
+          return;
+        }
+        this.setState({
+          menuitemdishes: response.data,
+          isFetching: true,
+        });
+      });
+  }
+
   render() {
+    const { menuitemdishes, isFetching } = this.state;
     return (
 
-      <Container className="menu">
+      <Container fluid className="menu">
         <Header />
-        <Dish />
+        <Container>
+          {isFetching ? (
+            <Row className="rows">
+              {menuitemdishes.map((menuitemdish) => (
+                <Dish key={menuitemdish.id} menuitemdish={menuitemdish} isFetching={isFetching} />
+              ))}
+            </Row>
+          ) : (
+            <Container className="spinner-container">
+              <ButtonToolbar className="justify-content-center">
+                <Spinner animation="border" variant="warning" />
+              </ButtonToolbar>
+            </Container>
+          )}
+        </Container>
       </Container>
 
     );
