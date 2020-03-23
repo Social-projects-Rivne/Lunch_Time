49ee47c9328e4b1f5b7d69f89b43995d8ecc7a5c
Voz4Nick <vmm408@gmail.com>
2020-03-23 13:24:06 +0200

    add methods on server for sorting restaurants

--- server/src/main/java/com/lunchtime/service/RestaurantService.java
+++ server/src/main/java/com/lunchtime/service/RestaurantService.java
@@ -14,6 +14,18 @@ public interface RestaurantService {
 
     Optional<Restaurant> findById(Long id);
 
+    Page<Restaurant> sortBy(Pageable pageable);
+
+    Page<Restaurant> chooseCity(Pageable pageable);
+
+    Page<Restaurant> chooseRating(Pageable pageable);
+
+    Page<Restaurant> chooseHours(Pageable pageable);
+
+    Page<Restaurant> yourPastVisits(Pageable pageable);
+
+    Page<Restaurant> chooseType(Pageable pageable);
+
     Restaurant update(Restaurant restaurant);
 
     Restaurant delete(Long id);
