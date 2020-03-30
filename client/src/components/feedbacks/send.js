ea02c24e24b26c524bfd5f18011ba690d331c327
narekacy <gurjiev42@gmail.com>
2020-03-30 20:05:17 +0300

    Add input to 'FeedbackSend' component'

--- client/src/components/feedbacks/send.js
+++ client/src/components/feedbacks/send.js
@@ -3,15 +3,39 @@ import { Button, Form } from 'react-bootstrap';
 import '../../styles/feedback-send.css';
 
 class FeedbackSend extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      val: '',
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit() {
+    console.log(this.state.val);
+    this.setState({ val: '' });
+  }
+
   render() {
     return (
-
       <Form.Group className="m-0">
-        <Form.Control className="textFeedback" as="textarea" rows="3" placeholder="feedback" />
-        <Button className="btnFormSend" variant="outline-success">Send Feedback</Button>
+        <Form.Control
+          className="textFeedback"
+          as="textarea"
+          rows="3"
+          placeholder="feedback"
+          value={this.state.val}
+          onChange={(e) => this.setState({ val: e.target.value })}
+          type="text"
+        />
+        <Button
+          className="btnFormSend"
+          variant="outline-success"
+          onClick={this.onSubmit}
+        >
+          Send Feedback
+        </Button>
       </Form.Group>
-
-
     );
   }
 }
