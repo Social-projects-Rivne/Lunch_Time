a7feaf9ee8cbae86db3cf24a98a28fd9e6eda34c
Roman Yevchuk <r.yevchuk@gmail.com>
2020-04-03 15:51:32 +0300

    Add close Alert func

--- client/src/components/shared/alert-base.js
+++ client/src/components/shared/alert-base.js
@@ -3,12 +3,24 @@ import Alert from 'react-bootstrap/Alert';
 import PropTypes from 'prop-types';
 
 class AlertBase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+    };
+  }
+
   render() {
     const { type, title } = this.props;
+    const { show } = this.state;
     return (
-      <Alert variant={type}>
-        <Alert.Heading>{title}</Alert.Heading>
-      </Alert>
+      <div>
+        {show && (
+        <Alert variant={type} onClose={() => this.setState({ show: false })} dismissible>
+          <Alert.Heading>{title}</Alert.Heading>
+        </Alert>
+        )}
+      </div>
     );
   }
 }
