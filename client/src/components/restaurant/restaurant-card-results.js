614fc8ef493597d958f08fabaf01a475f303a118
Voz4Nick <vmm408@gmail.com>
2020-04-04 01:34:56 +0300

    edit restaurant card result file

--- client/src/components/restaurant/restaurant-card-results.js
+++ client/src/components/restaurant/restaurant-card-results.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { CardDeck, Container } from 'react-bootstrap';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
-import Spinner from 'react-bootstrap/Spinner';
 import RestaurantCardView from './restaurant-card-view';
 
 class RestaurantCardResults extends Component {
@@ -17,24 +15,11 @@ class RestaurantCardResults extends Component {
     );
   }
 
-  initButtonToolbar() {
-    const {
-      spinnerContainerClassName, buttonToolbarClassName, spinnerAnimation, spinnerVariant,
-    } = this.props;
-    return (
-      <Container className={spinnerContainerClassName}>
-        <ButtonToolbar className={buttonToolbarClassName}>
-          <Spinner animation={spinnerAnimation} variant={spinnerVariant} />
-        </ButtonToolbar>
-      </Container>
-    );
-  }
-
   render() {
-    const { restaurantContainerClassName, isFetching } = this.props;
+    const { restaurantContainerClassName } = this.props;
     return (
       <Container className={restaurantContainerClassName}>
-        {isFetching ? this.initRestaurantsCards() : this.initButtonToolbar()}
+        {this.initRestaurantsCards()}
       </Container>
     );
   }
@@ -43,21 +28,12 @@ class RestaurantCardResults extends Component {
 RestaurantCardResults.defaultProps = {
   restaurantContainerClassName: 'card-body pl-5 pr-5',
   cardDeckClassName: 'wrapper',
-  spinnerContainerClassName: 'spinner-container',
-  buttonToolbarClassName: 'justify-content-center',
-  spinnerAnimation: 'border',
-  spinnerVariant: 'warning',
 };
 
 RestaurantCardResults.propTypes = {
-  isFetching: PropTypes.bool.isRequired,
   restaurants: PropTypes.array.isRequired,
   restaurantContainerClassName: PropTypes.string,
   cardDeckClassName: PropTypes.string,
-  spinnerContainerClassName: PropTypes.string,
-  buttonToolbarClassName: PropTypes.string,
-  spinnerAnimation: PropTypes.string,
-  spinnerVariant: PropTypes.string,
 };
 
 export default RestaurantCardResults;
