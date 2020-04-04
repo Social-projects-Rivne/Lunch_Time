7117513495879b6e28d056ff3d5f8ba46eb8b4ba
Voz4Nick <vmm408@gmail.com>
2020-04-04 13:30:50 +0300

    make restaurant list show pages

--- client/src/pages/restaurant-list.js
+++ client/src/pages/restaurant-list.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
+import Pagination from 'react-bootstrap-pagination-logic';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Spinner from 'react-bootstrap/Spinner';
 import Api from '../services/api';
 import RestaurantCardResults from '../components/restaurant/restaurant-card-results';
 
@@ -7,39 +10,89 @@ class ListRestaurant extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      totalPages: 0,
+      number: 0,
       restaurants: [],
       isFetching: false,
     };
+    this.handlePageChange = this.handlePageChange.bind(this);
   }
 
   componentDidMount() {
-    this.getAll();
+    this.getAll(this.state.number);
   }
 
-  getAll() {
-    Api.getAll('restaurants')
+  getAll(props) {
+    Api.getAll(`restaurants?page=${props}`)
       .then((response) => {
-        return response.error ? this.initError(response) : this.initState(response);
+        if (response.error) {
+          // eslint-disable-next-line no-console
+          console.error(response);
+          return;
+        }
+        this.initState(response);
       });
-  }
-
-  initError(response) {
-    // eslint-disable-next-line no-console
-    console.error(response);
   }
 
   initState(response) {
     this.setState({
-      restaurants: response.data,
+      totalPages: response.data.totalPages,
+      number: response.data.number,
+      restaurants: response.data.content,
       isFetching: true,
     });
   }
 
+  handlePageChange(page) {
+    this.getAll(page - 1);
+  }
+
+  initRestaurantResultCard() {
+    const { restaurants } = this.state;
+    return (
+      <RestaurantCardResults restaurants={restaurants} />
+    );
+  }
+
+  initButtonToolbar() {
+    return (
+      <Container className="spinner-container">
+        <ButtonToolbar className="justify-content-center">
+          <Spinner animation="border" variant="warning" />
+        </ButtonToolbar>
+      </Container>
+    );
+  }
+
+  initPagination() {
+    const { number, totalPages } = this.state;
+    if (totalPages === 1) {
+      return;
+    }
+    // eslint-disable-next-line consistent-return
+    return (
+      <Pagination
+        current_page={number + 1}
+        last_page={totalPages}
+        position="center"
+        handlePageChange={this.handlePageChange}
+      />
+    );
+  }
+
   render() {
-    const { restaurants, isFetching } = this.state;
+    const { isFetching } = this.state;
+    if (isFetching) {
+      return (
+        <Container fluid className="page-container p-0">
+          {this.initRestaurantResultCard()}
+          {this.initPagination()}
+        </Container>
+      );
+    }
     return (
       <Container fluid className="page-container p-0">
-        <RestaurantCardResults restaurants={restaurants} isFetching={isFetching} />
+        {this.initButtonToolbar()}
       </Container>
     );
   }
