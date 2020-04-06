107e587ec6172ff661c6593fa9763320c24ee275
Andrii_Melnyk <andriy24681995@gmail.com>
2020-04-06 15:47:23 +0300

    navigation when loged in

---
+++ client/src/components/info/lognavigation.js
@@ -0,0 +1,11 @@
+const lognavigation = [
+  { link: '/restaurants', name: 'ListRestaurant' },
+  { link: '/events', name: 'Events' },
+  { link: '/map', name: 'Map' },
+  { link: '/about', name: 'About' },
+  { link: '/contact', name: 'Contact' },
+  { link: '/profile', name: 'Profile' },
+  { link: '/', name: 'Logout' },
+];
+
+export default lognavigation;
