d36ae846729bef75786f3a5e7d19f3f4ef9e2114
Andrii_Melnyk <andriy24681995@gmail.com>
2020-04-07 14:01:05 +0300

    fix empty line

--- server/src/main/java/com/lunchtime/controllers/FeedbackController.java
+++ server/src/main/java/com/lunchtime/controllers/FeedbackController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 //TODO remove redundant lines
-
 @RestController
 @RequestMapping("/api/feedback")
 public class FeedbackController {
