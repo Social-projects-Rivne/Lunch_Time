56bced9dddd42958212d958f21d621d415ad20a8
Andrii_Melnyk <andriy24681995@gmail.com>
2020-04-07 13:53:52 +0300

    remove TODO FROM LUNCHTIME

--- server/src/main/java/com/lunchtime/LunchTimeApplication.java
+++ server/src/main/java/com/lunchtime/LunchTimeApplication.java
@@ -1,6 +1,5 @@
 package com.lunchtime;
 
-//TODO remove unused imports. You can use Ctrl+Alt+O in Intellij before committing changes
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
