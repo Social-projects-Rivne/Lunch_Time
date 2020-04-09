d735bd171cc153db52e0b4ab57515121aa64025b
narekacy <gurjiev42@gmail.com>
2020-04-09 23:57:17 +0300

    Remove unnecessary data from header

--- client/src/components/info/header.js
+++ client/src/components/info/header.js
@@ -2,8 +2,6 @@ const info = [
   { link: '/restaurants', name: 'Restaurants' },
   { link: '/events', name: 'Events' },
   { link: '/map', name: 'Map' },
-  { link: '/about', name: 'About' },
-  { link: '/contact', name: 'Contact' },
   { link: '/login', name: 'Login' },
   { link: '/registration', name: 'Registration' },
   { link: '/profile', name: 'Profile' },
