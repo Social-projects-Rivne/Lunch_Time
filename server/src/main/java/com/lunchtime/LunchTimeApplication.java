26aaea14985918ccce2be42bda15872fb775d5dd
Roman Yevchuk <r.yevchuk@gmail.com>
2020-04-09 13:58:04 +0300

    Fix images location

--- server/src/main/java/com/lunchtime/LunchTimeApplication.java
+++ server/src/main/java/com/lunchtime/LunchTimeApplication.java
@@ -1,21 +1,13 @@
 package com.lunchtime;
 
-//TODO remove unused imports. You can use Ctrl+Alt+O in Intellij before committing changes
-import com.lunchtime.models.Restaurant;
-import com.lunchtime.repository.RestaurantRepository;
-import com.lunchtime.service.RestaurantService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(
-    //TODO One time will be enough
-    exclude = {SecurityAutoConfiguration.class, SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class LunchTimeApplication {
 
     public static void main(String[] args) {
@@ -35,9 +27,12 @@ public class LunchTimeApplication {
                     // this list without creating new PR, for example for prod env, you will need only ssh
                     .allowedOrigins("http://localhost:3000");
             }
+
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/api/**")
+                    .addResourceLocations("classpath:/static/");
+            }
         };
     }
-
-
-
 }
