06112dd3fce9b26bf291bddc9ea6f6340bc3af05
narekacy <gurjiev42@gmail.com>
2020-04-11 23:00:30 +0300

    Add 'Registration' page component

--- client/src/pages/registration.js
+++ client/src/pages/registration.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
-
+import { Container } from 'react-bootstrap';
+import Login from '../components/login/login';
+import Register from '../components/login/register';
+import RightSide from '../components/login/right-side';
+import '../styles/register-login-page.css';
 
 class Registration extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogged: false,
+    };
+    this.changeState = this.changeState.bind(this);
+  }
+
+  componentDidMount() {
+    this.rightSide.classList.add('right');
+  }
+
+  changeState() {
+    const { isLogged } = this.state;
+    if (isLogged) {
+      this.rightSide.classList.remove('left');
+      this.rightSide.classList.add('right');
+    } else {
+      this.rightSide.classList.remove('right');
+      this.rightSide.classList.add('left');
+    }
+    this.setState((prevState) => ({ isLogged: !prevState.isLogged }));
+  }
+
   render() {
+    const { isLogged } = this.state;
+    const current = isLogged ? 'Register' : 'Login';
+    const currentActive = isLogged ? 'login' : 'register';
     return (
-      <h1>Registration Page</h1>
+      <Container className="mainContainer">
+        <div className="login">
+          <div className="container" ref={(ref) => { this.container = ref; }}>
+            {isLogged && <Login />}
+            {!isLogged && <Register />}
+          </div>
+          <RightSide
+            current={current}
+            currentActive={currentActive}
+            containerRef={(ref) => { this.rightSide = ref; }}
+            onClick={this.changeState}
+          />
+        </div>
+      </Container>
     );
   }
 }
