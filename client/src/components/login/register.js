c5eeab4fe2a6483fc79a8eeb946d10171887a5f5
narekacy <gurjiev42@gmail.com>
2020-04-12 19:30:38 +0300

    Rename image variable

--- client/src/components/login/register.js
+++ client/src/components/login/register.js
@@ -2,91 +2,62 @@ import React, { Component } from 'react';
 import {
   Container, Form, FormGroup, FormLabel,
 } from 'react-bootstrap';
-import loginImg from './register.png';
+import regImg from './register.png';
 
 class Register extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      email: '',
-      password: '',
-      passwordConfirm: '',
     };
   }
 
-  onChangeEmail(e) {
-    this.setState({
-      email: e.target.value,
-    });
-  }
-
-  onChangePassword(e) {
-    this.setState({
-      password: e.target.value,
-    });
-  }
-
-  onChangePasswordConfirm(e) {
-    this.setState({
-      passwordConfirm: e.target.value,
-    });
-  }
-
   render() {
-    const { email, password, passwordConfirm } = this.state;
     return (
       <Container className="base-container" style={{ color: '#3498db' }}>
         <div className="header">Register</div>
         <div className="content">
           <div className="image">
-            <img src={loginImg} alt="login" />
+            <img src={regImg} alt="register" />
           </div>
-          <Form className="form">
+          <Form className="form" onSubmit={this.handleSubmit}>
             <FormGroup>
               <FormLabel htmlFor="text">Name</FormLabel>
               <input
                 type="text"
                 name="name"
                 placeholder="name"
-                value={passwordConfirm}
-                onChange={(e) => this.onChangePasswordConfirm(e)}
-              />
-              <FormLabel htmlFor="password">Phone number</FormLabel>
-              <input
-                type="text"
-                name="phone number"
-                placeholder="phone number"
-                value={passwordConfirm}
-                onChange={(e) => this.onChangePasswordConfirm(e)}
+                noValidate
+                onChange={this.handleChange}
               />
               <FormLabel htmlFor="email">e-mail</FormLabel>
               <input
                 type="email"
                 name="email"
                 placeholder="email"
-                value={email}
-                onChange={(e) => this.onChangeEmail(e, email)}
+                noValidate
+                onChange={this.handleChange}
               />
               <FormLabel htmlFor="password">Password</FormLabel>
               <input
                 type="password"
                 name="password"
                 placeholder="password"
-                value={password}
-                onChange={(e) => this.onChangePassword(e)}
+                noValidate
+                onChange={this.handleChange}
               />
               <input
                 type="password"
-                name="password"
+                name="confirmPassword"
                 placeholder="confirm password"
-                value={passwordConfirm}
-                onChange={(e) => this.onChangePasswordConfirm(e)}
+                noValidate
+                onChange={this.handleChange}
               />
             </FormGroup>
           </Form>
         </div>
         <div className="footer">
-          <button type="button" className="btn-reg">
+          <button type="submit" className="btn-reg">
             Register
           </button>
         </div>
