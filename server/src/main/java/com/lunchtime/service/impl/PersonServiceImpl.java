39bd9e76fbca4e8c39cd81764fb582deea32c16c
narekacy <gurjiev42@gmail.com>
2020-04-14 00:56:12 +0300

    Modify save() method in PersonServiceImpl

--- server/src/main/java/com/lunchtime/service/impl/PersonServiceImpl.java
+++ server/src/main/java/com/lunchtime/service/impl/PersonServiceImpl.java
@@ -2,27 +2,28 @@ package com.lunchtime.service.impl;
 
 import com.lunchtime.models.Person;
 import com.lunchtime.repository.PersonRepository;
+import com.lunchtime.service.PersonDto;
+import com.lunchtime.service.PersonMapper;
 import com.lunchtime.service.PersonService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class PersonServiceImpl implements PersonService {
 
     private final PersonRepository personRepository;
+    private final PersonMapper personMapper;
 
-    public PersonServiceImpl(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
-
-    public Person save(Person person) {
-        return personRepository.save(person);
+    public PersonDto save(PersonDto personDto) {
+        Person person = personMapper.fromDtoToPerson(personDto);
+        personRepository.save(person);
+        return personMapper.fromPersonToDto(person);
     }
 
     public Optional<Person> findById(Long id) {
         return personRepository.findById(id);
     }
-
-
 }
