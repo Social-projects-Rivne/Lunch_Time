560e50221e3648f4511139936e9bea0be103a0cf
Roman Yevchuk <r.yevchuk@gmail.com>
2020-04-13 13:08:40 +0300

    Add path /api/images/ to WebSecurityConfig

--- server/src/main/java/com/lunchtime/config/WebConfiguration.java
+++ server/src/main/java/com/lunchtime/config/WebConfiguration.java
@@ -1,4 +1,4 @@
-package com.lunchtime;
+package com.lunchtime.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
