aa3e3f9ba5efc8843133471bc8cd5894899a56d9
narekacy <gurjiev42@gmail.com>
2020-04-14 00:50:03 +0300

    Add 'PersonMapper'

---
+++ server/src/main/java/com/lunchtime/service/PersonMapper.java
@@ -0,0 +1,28 @@
+package com.lunchtime.service;
+
+import com.lunchtime.models.Person;
+import com.lunchtime.repository.PersonRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class PersonMapper {
+    final PersonRepository personRepository;
+
+    public Person fromDtoToPerson(PersonDto personDto) {
+        Person person = new Person();
+        person.setName(personDto.getName());
+        person.setPhoneNumber(personDto.getPhoneNumber());
+        person.setEmail(personDto.getEmail());
+        person.setPassword(personDto.getPassword());
+        return person;
+    }
+
+    public PersonDto fromPersonToDto(Person person) {
+        PersonDto personDto = new PersonDto();
+        personDto.setId(person.getId());
+        personDto.setName(person.getName());
+        personDto.setPhoneNumber(person.getPhoneNumber());
+        personDto.setEmail(person.getEmail());
+        return personDto;
+    }
+}
