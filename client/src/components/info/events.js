87d33c2465aa4156f194cd4c0eea7f9ca46e155b
narekacy <gurjiev42@gmail.com>
2020-04-14 18:57:09 +0300

    Events header fix

--- client/src/components/info/events.js
+++ client/src/components/info/events.js
@@ -17,7 +17,7 @@ const info = [
   },
 ];
 
-export const title = 'All events and activities in restaurants, cafes and bars';
+export const title = 'EVENTS';
 export const placeHolder = 'Search events';
 
 export default info;
