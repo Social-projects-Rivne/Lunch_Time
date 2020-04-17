929b7ed0e5a4dd507b5f06b7ab3926c515df7809
Vitalii Makarchenkov <vitalii.mak@gmail.com>
2020-04-17 17:45:22 +0300

    Add failed test

--- server/src/test/java/com/lunchtime/service/impl/OrderServiceImplTest.java
+++ server/src/test/java/com/lunchtime/service/impl/OrderServiceImplTest.java
@@ -13,18 +13,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class OrderServiceImplTest {
 
     @InjectMocks
@@ -40,8 +38,10 @@ public class OrderServiceImplTest {
     private OrderStatusService orderStatusService;
 
     private RestaurantTable restaurantTable;
-    private Order order;
     private Person person;
+    private final Long personId = 1L;
+    private final Long tableId = 1L;
+    private final int tableCapacity = 4;
 
     @Before
     public void setup() {
@@ -52,13 +52,16 @@ public class OrderServiceImplTest {
             .build();
 
         person = new Person();
-        person.setId(1L);
+        person.setId(personId);
 
-        restaurantTable = new RestaurantTable();
-        restaurantTable.setId(1L);
-        restaurantTable.setCapacity(4);
+        restaurantTable = RestaurantTable.builder()
+            .id(tableId)
+            .capacity(tableCapacity)
+            .build();
 
         when(orderStatusService.getOrderStatusByName(statusName)).thenReturn(Optional.of(status));
+        when(personService.getPersonById(personId)).thenReturn(Optional.of(person));
+        when(restaurantTableService.getTableById(tableId)).thenReturn(Optional.of(restaurantTable));
     }
 
     @Test
@@ -68,7 +71,7 @@ public class OrderServiceImplTest {
         Date startDate = Date.from(start.atZone(ZoneId.systemDefault()).toInstant());
         Date finishDate = Date.from(finish.atZone(ZoneId.systemDefault()).toInstant());
 
-        order = Order.builder()
+        Order order = Order.builder()
             .table(restaurantTable)
             .person(person)
             .startTime(startDate)
@@ -78,13 +81,38 @@ public class OrderServiceImplTest {
 
         when(orderRepository.findAllOrdersByTableInTime(order.getTable().getId(), startDate, finishDate))
             .thenReturn(Collections.emptyList());
-        when(personService.getPersonById(order.getPerson().getId())).thenReturn(Optional.of(person));
-        when(restaurantTableService.getTableById(order.getTable().getId())).thenReturn(Optional.of(restaurantTable));
         when(orderRepository.save(order)).thenReturn(order);
 
         Order createdOrder = orderServiceImpl.saveOrder(order);
 
         assertNotNull(createdOrder);
         assertEquals(order, createdOrder);
+    }
+
+    @Test
+    public void personCanNotMakeOrderIfThereAreOtherOrdersInThatTimeInThatTableInThatRestaurant() {
+        LocalDateTime start = LocalDateTime.now().plusDays(2);
+        LocalDateTime finish = LocalDateTime.now().plusMinutes(40).plusDays(2);
+        Date startDate = Date.from(start.atZone(ZoneId.systemDefault()).toInstant());
+        Date finishDate = Date.from(finish.atZone(ZoneId.systemDefault()).toInstant());
+
+        Order order = Order.builder()
+            .table(restaurantTable)
+            .person(person)
+            .startTime(startDate)
+            .finishTime(finishDate)
+            .visitors(3)
+            .build();
+
+        List<Order> orders = new ArrayList<>();
+        orders.add(Order.builder().build());
+
+        when(orderRepository.findAllOrdersByTableInTime(order.getTable().getId(), startDate, finishDate))
+            .thenReturn(orders);
+        when(orderRepository.save(order)).thenReturn(order);
+
+        Order createdOrder = orderServiceImpl.saveOrder(order);
+
+        assertNull(createdOrder);
     }
 }
