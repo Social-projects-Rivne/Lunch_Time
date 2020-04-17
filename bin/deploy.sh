200599a67133063099129c4ad20f843c688f2877
Vitalii Makarchenkov <vitalii.mak@gmail.com>
2020-04-17 11:01:20 +0300

    Fix error logging

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 log_file="$(date +'%B%d-%H%M%S')".log
+log_error_file="$(date +'%B%d-%H%M%S')"-error.log
 log_folder=/home/github/logs
 
 cd /home/github/Lunch_Time
@@ -10,11 +11,11 @@ echo "$(git log --name-status HEAD^..HEAD)" >> $log_folder/$log_file
 echo "==== docker ps ====" >> $log_folder/$log_file
 echo "$(docker ps)" >> $log_folder/$log_file
 
-docker-compose -f docker-compose.prod.yml stop >> $log_folder/$log_file
+docker-compose -f docker-compose.prod.yml stop 1>> $log_folder/$log_file 2>> $log_folder/$log_error_file
 echo "==== docker ps after stop ====" >> $log_folder/$log_file
 echo "$(docker ps)" >> $log_folder/$log_file
 sleep 5
 
-docker-compose -f docker-compose.prod.yml up -d --build >> $log_folder/$log_file
+docker-compose -f docker-compose.prod.yml up -d --build 1>> $log_folder/$log_file 2>> $log_folder/$log_error_file
 echo "==== docker ps after run ====" >> $log_folder/$log_file
 echo "$(docker ps)" >> $log_folder/$log_file
