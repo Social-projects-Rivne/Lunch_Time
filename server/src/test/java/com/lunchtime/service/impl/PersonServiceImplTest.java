66bc7217492a4153f180abc190b13cd5d70f910e
narekacy <gurjiev42@gmail.com>
2020-04-20 17:54:48 +0300

    Add unit tests

---
+++ server/src/test/java/com/lunchtime/service/impl/PersonServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.lunchtime.service.impl;
+
+import com.lunchtime.models.Person;
+import com.lunchtime.repository.PersonRepository;
+import com.lunchtime.service.dto.RegisterPerson;
+import com.lunchtime.stub.PersonMapperStub;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class PersonServiceImplTest {
+
+    @Mock
+    PersonRepository personRepository;
+
+    private final RegisterPerson registerPerson = new RegisterPerson();
+    private final PersonMapperStub personMapperStub = new PersonMapperStub();
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        registerPerson.setName("Name");
+        registerPerson.setPhoneNumber("+380501234567");
+        registerPerson.setEmail("test@gmail.com");
+        registerPerson.setPassword("123456Qwerty");
+    }
+
+    @Test
+    public void save_valid_person_and_return_not_null_dto() throws Exception {
+        Person mappedPerson = personMapperStub.fromRegisterToPerson(registerPerson);
+        personRepository.save(mappedPerson);
+        when(personRepository.save(mappedPerson)).thenReturn(mappedPerson);
+        Person savedPerson = personRepository.save(mappedPerson);
+        Assert.assertNotNull(savedPerson);
+    }
+}
---
+++ server/src/test/java/com/lunchtime/stub/PersonServiceStub.java
@@ -0,0 +1,55 @@
+package com.lunchtime.stub;
+
+import com.lunchtime.models.Person;
+import com.lunchtime.service.PersonService;
+import com.lunchtime.service.dto.PersonDto;
+import com.lunchtime.service.dto.PersonPassDto;
+import com.lunchtime.service.dto.RegisterPerson;
+import java.util.Optional;
+
+public class PersonServiceStub implements PersonService {
+
+    @Override
+    public PersonDto saveRegisterPerson(RegisterPerson registerPerson) {
+        if (registerPerson != null) {
+            return new PersonDto();
+        }
+        return null;
+    }
+
+    @Override
+    public Person findByEmail(String email) {
+        if (email.equals("test@gmail.com")) {
+            return new Person();
+        }
+        return null;
+    }
+
+    @Override
+    public Person findByPhoneNumber(String phoneNumber) {
+        if (phoneNumber.equals("+380501234567")) {
+            return new Person();
+        }
+        return null;
+    }
+
+    @Override
+    public PersonDto updatePerson(PersonDto personDto) {
+        return null;
+    }
+
+    @Override
+    public PersonPassDto updatePassword(PersonPassDto personPassDto) throws Exception {
+        return null;
+    }
+
+    @Override
+    public PersonDto getPersonDtoById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Optional<Person> getPersonById(Long id) {
+        return Optional.empty();
+    }
+}
