979b00afc265ee3c91f7820aa2a770ce04364bfc
Roman Yevchuk <r.yevchuk@gmail.com>
2020-04-23 10:53:17 +0300

    Remove link element from restaurant items

--- client/src/components/restaurant/restaurant-card-view.js
+++ client/src/components/restaurant/restaurant-card-view.js
@@ -8,22 +8,22 @@ class RestaurantCardView extends Component {
     const { restaurant } = this.props;
     const link = `/restaurants/${restaurant.id}`;
     return (
-      <Card className="m-2" border="dark">
-        <Link to={link}>
-          <Card.Body>
+      <Card className="text-dark m-2" border="dark">
+        <Card.Body>
+          <Link to={link}>
             <Card.Title className="">{restaurant.name}</Card.Title>
-            <Card.Subtitle className="restaurant-name mb-2">
-              Hours:
-              {' '}
-              {restaurant.workingTime}
-            </Card.Subtitle>
-            <Card.Subtitle className="restaurant-name mb-2">
-              Address:
-              {' '}
-              {restaurant.textAddress}
-            </Card.Subtitle>
-          </Card.Body>
-        </Link>
+          </Link>
+          <Card.Subtitle className="restaurant-name mb-2 black">
+            Hours:
+            {' '}
+            {restaurant.workingTime}
+          </Card.Subtitle>
+          <Card.Subtitle className="restaurant-name mb-2">
+            Address:
+            {' '}
+            {restaurant.textAddress}
+          </Card.Subtitle>
+        </Card.Body>
       </Card>
     );
   }
