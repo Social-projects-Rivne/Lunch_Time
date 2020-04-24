a01d94017dba9fc96e5c46c61c54b15883cb9020
Andriy_Melnyk <kevin.straik@gmail.com>
2020-04-24 13:23:08 +0300

    start

---
+++ client/src/components/restaurant/restaurant-register.js
@@ -0,0 +1,11 @@
+
+// eslint-disable-next-line no-undef
+class RestaurantRegistration extends Component {
+  render() {
+    return (
+      // eslint-disable-next-line react/react-in-jsx-scope
+      <h1>RestaurantRegistration</h1>
+
+    );
+  }
+} export default RestaurantRegistration;
