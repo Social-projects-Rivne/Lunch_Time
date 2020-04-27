d70524db976ff5fd27087dae97e3fb4366b624af
Roman Yevchuk <r.yevchuk@gmail.com>
2020-04-27 14:13:45 +0300

    Fix checkstyle error

--- server/src/main/java/com/lunchtime/service/impl/ImageServiceImpl.java
+++ server/src/main/java/com/lunchtime/service/impl/ImageServiceImpl.java
@@ -11,8 +11,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-@Service
 
+@Service
 public class ImageServiceImpl implements ImageService {
 
     public boolean saveImage(MultipartFile file, String endpoint) {
