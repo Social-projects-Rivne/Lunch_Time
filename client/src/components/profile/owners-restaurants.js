f7326f54f6b85543b23c4b5964590ea01617b80b
Voz4Nick <vmm408@gmail.com>
2020-04-28 16:28:52 +0300

    add list UI to owner page. Add restaurants as cards

--- client/src/components/profile/owners-restaurants.js
+++ client/src/components/profile/owners-restaurants.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Spinner } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import Pagination from 'react-bootstrap-pagination-logic';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Card from 'react-bootstrap/Card';
 import Api from '../../services/api';
 
 class OwnersRestaurants extends Component {
@@ -8,9 +11,13 @@ class OwnersRestaurants extends Component {
     super(props);
     this.state = {
       userId: props.userId,
+      totalPages: 0,
       number: 0,
-      pageSize: 5,
+      pageSize: 10,
+      restaurants: [],
+      isFetching: false,
     };
+    this.handlePageChange = this.handlePageChange.bind(this);
   }
 
   componentDidMount() {
@@ -31,14 +38,71 @@ class OwnersRestaurants extends Component {
 
   initState(response) {
     this.setState({
+      totalPages: response.data.totalPages,
       number: response.data.number,
+      restaurants: response.data.content,
+      isFetching: true,
     });
   }
 
-  render() {
+  handlePageChange(page) {
+    this.getAll(this.state.userId, page - 1, this.state.pageSize);
+  }
+
+  initRestaurantResultCard() {
+    const { restaurants } = this.state;
     return (
-      <Container fluid>
-        My Restaurants
+      <Container className="card-body pl-5 pr-5">
+        {restaurants.map((restaurant) => (
+          <Card className="m-2" border="dark">
+            <Card.Body>
+              <Card.Title>{restaurant.name}</Card.Title>
+              <Card.Subtitle>{restaurant.textAddress}</Card.Subtitle>
+            </Card.Body>
+          </Card>
+        ))}
+      </Container>
+    );
+  }
+
+  initButtonToolbar() {
+    return (
+      <Container className="spinner-container">
+        <ButtonToolbar className="justify-content-center">
+          <Spinner animation="border" variant="warning" />
+        </ButtonToolbar>
+      </Container>
+    );
+  }
+
+  initPagination() {
+    const { number, totalPages } = this.state;
+    if (totalPages === 1) {
+      return null;
+    }
+    return (
+      <Pagination
+        current_page={number + 1}
+        last_page={totalPages}
+        position="center"
+        handlePageChange={this.handlePageChange}
+      />
+    );
+  }
+
+  render() {
+    const { isFetching } = this.state;
+    if (isFetching) {
+      return (
+        <Container fluid className="restaurant-list-container">
+          {this.initRestaurantResultCard()}
+          {this.initPagination()}
+        </Container>
+      );
+    }
+    return (
+      <Container fluid className="restaurant-list-container">
+        {this.initButtonToolbar()}
       </Container>
     );
   }
