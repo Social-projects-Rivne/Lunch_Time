523c8e1f3192b3d9e4777033e97f2e947ff542ae
narekacy <gurjiev42@gmail.com>
2020-05-02 01:08:44 +0300

    Add 'MailConfig'

---
+++ server/src/main/java/com/lunchtime/config/MailConfig.java
@@ -0,0 +1,48 @@
+package com.lunchtime.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${spring.mail.host}")
+    String host;
+
+    @Value("${spring.mail.userName}")
+    String userName;
+
+    @Value("${spring.mail.password}")
+    String password;
+
+    @Value("${spring.mail.port}")
+    int port;
+
+    @Value("${spring.mail.protocol}")
+    String protocol;
+
+    @Value("${spring.mail.debug}")
+    String debug;
+
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(userName);
+        mailSender.setPassword(password);
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.setProperty("mail.transport.protocol", protocol);
+        properties.setProperty("mail.debug", debug);
+
+
+        return mailSender;
+    }
+}
