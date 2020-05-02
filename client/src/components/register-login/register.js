6f199e31730fe664fec4d8a338bd1b67ee13aa77
narekacy <gurjiev42@gmail.com>
2020-05-02 23:08:15 +0300

    Modify password title for better UX

--- client/src/components/register-login/register.js
+++ client/src/components/register-login/register.js
@@ -52,7 +52,7 @@ class Register extends Component {
       showStrong: false,
       passwordStrength: '',
       passwordInputStarted: false,
-      passwordInputTitle: "Password shouldn't be weak and less than 8 symbols",
+      passwordInputTitle: 'We recommend setting strong password with 8 or more symbols',
       passwordInputClassName: '',
       passwordInputWrongClassName: false,
       confirmPasswordInputTitle: 'Passwords must match each other',
