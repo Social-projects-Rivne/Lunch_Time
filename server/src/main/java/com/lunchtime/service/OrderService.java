5f319ad66056445cd2adca420370816c72390d6f
narekacy <gurjiev42@gmail.com>
2020-05-02 17:18:31 +0300

    Modify OrderService

--- server/src/main/java/com/lunchtime/service/OrderService.java
+++ server/src/main/java/com/lunchtime/service/OrderService.java
@@ -1,13 +1,14 @@
 package com.lunchtime.service;
 
 import com.lunchtime.models.Order;
+import com.lunchtime.service.dto.OrderDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface OrderService {
-    Order saveOrder(Order order);
+    OrderDto saveOrder(OrderDto orderDto);
 
     Page<Order> getAllOrders(Pageable pageable);
 
