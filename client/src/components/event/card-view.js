aedf9eabe3f073106cf96fea087908199048930b
narekacy <gurjiev42@gmail.com>
2020-05-04 19:30:25 +0300

    Fix events link availability

--- client/src/components/event/card-view.js
+++ client/src/components/event/card-view.js
@@ -2,17 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import moment from 'moment';
+import { Link } from 'react-router-dom';
 import Api from '../../services/api';
 
 class CardView extends React.Component {
   render() {
-    const { event } = this.props;
+    const { event, showLink } = this.props;
+    console.log(showLink);
     const img = `${Api.apiUrl}images/events/${event.image}`;
+    const link = `/restaurants/${event.restaurant.id}`;
     return (
       <Card className="text-dark m-2" border="dark">
         <Card.Img variant="top" src={img} alt="Event image" />
         <Card.Body className="">
-          <Card.Title className="" style={{ color: '#1e1e1e' }}>{event.name.toUpperCase()}</Card.Title>
+          {showLink && <Link to={link}><Card.Title className="">{event.name.toUpperCase()}</Card.Title></Link>}
+          {!showLink && <Card.Title className="" style={{ color: '#1e1e1e' }}>{event.name.toUpperCase()}</Card.Title>}
           <Card.Subtitle className="font-weight-bold mb-2">{event.restaurant.name}</Card.Subtitle>
           <Card.Subtitle className="event-date mb-2 text-muted">
             {moment(event.date).format('DD.MM.YYYY HH:mm')}
@@ -24,8 +28,13 @@ class CardView extends React.Component {
   }
 }
 
+CardView.defaultProps = {
+  showLink: false,
+};
+
 CardView.propTypes = {
   event: PropTypes.shape([]).isRequired,
+  showLink: PropTypes.bool,
 };
 
 export default CardView;
