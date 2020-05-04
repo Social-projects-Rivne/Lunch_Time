26f1d02b8b21a041bd4085db0e41cb7781003816
Andrii Nazaruk <andriy_nazaruk@yahoo.de>
2020-05-04 05:59:23 -0700

    add png file and create examole of a marker

--- client/src/components/shared/openlayers-map.js
+++ client/src/components/shared/openlayers-map.js
@@ -4,8 +4,32 @@ import { Map, layer, Layers } from 'react-openlayers';
 import { Container } from 'react-bootstrap';
 import { transform } from 'ol/proj';
 import '../../styles/openlayers-map.css';
+import * as ol from 'openlayers';
+import { Icon, Style } from 'ol/style';
 
 class OpenlayersMap extends Component {
+  constructor(props) {
+    super(props);
+    this.newVector = this.newVector.bind(this);
+  }
+
+  newVector() {
+    const iconFeature = new ol.Feature(new ol.geom.Point([50.616294, 26.275728]));
+    const iconStyle = new Style({
+      image: new Icon({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        src: '../../map-pin-point.png',
+      }),
+    });
+    iconFeature.setStyle(iconStyle);
+    const source = new ol.source.Vector({ features: [iconFeature] });
+    return (
+      <layer.Vector source={source} />
+    );
+  }
+
   render() {
     const { latitude, longitude } = this.props;
     return (
@@ -20,6 +44,7 @@ class OpenlayersMap extends Component {
         >
           <Layers>
             <layer.Tile />
+            {this.newVector()}
           </Layers>
         </Map>
       </Container>
