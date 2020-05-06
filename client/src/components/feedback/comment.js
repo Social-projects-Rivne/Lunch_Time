b33f3b85089e0502bccf156340e4b5eb80ed41ec
narekacy <gurjiev42@gmail.com>
2020-05-06 23:03:47 +0300

    Modify logic for likes counter refreshind on frontend

--- client/src/components/feedback/comment.js
+++ client/src/components/feedback/comment.js
@@ -3,13 +3,16 @@ import { Container, Image } from 'react-bootstrap';
 import '../../styles/feedback-comment.css';
 import PropTypes from 'prop-types';
 import Api from '../../services/api';
+import Person from '../../services/person';
 
 class FeedbackComment extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      item: this.props.item,
       avatar: '/img/default-avatar.png',
     };
+    this.likeFeedback = this.likeFeedback.bind(this);
   }
 
   componentDidMount() {
@@ -29,18 +32,35 @@ class FeedbackComment extends Component {
     }
   }
 
+  likeFeedback() {
+    Api.post(`feedback/like?feedbackId=${this.props.item.id}&personId=${Person.userInfo.id}`)
+      .then((response) => {
+        if (response.error == null) {
+          const { item } = this.state;
+          if (item.id === response.data.id) {
+            this.setState({
+              item: response.data,
+            });
+          }
+        } else {
+          // eslint-disable-next-line no-console
+          console.log(response.error);
+        }
+      });
+  }
+
   render() {
-    const { item } = this.props;
+    const { item } = this.state;
     return (
       <Container className="feedbackContainer">
         <Image src={this.state.avatar} width="50px" height="50px" alt="defAvava" roundedCircle />
         <div className="cooment">
-          <strong className="name">{item.person.name}</strong>
+          <strong className="name">{item.personName}</strong>
           <small className="date">
             {
-            `${item.date.substring(8, 10)}/${item.date.substring(5, 7)}/${item.date.substring(0, 4)}`
-            + ` ${item.date.substring(11, 16)}`
-          }
+              `${item.date.substring(8, 10)}/${item.date.substring(5, 7)}/${item.date.substring(0, 4)}`
+              + ` ${item.date.substring(11, 16)}`
+            }
           </small>
           <hr className="hrStyle" />
           <div>{item.description}</div>
@@ -49,8 +69,18 @@ class FeedbackComment extends Component {
 
             <span className="answer" title="answer to feedback">answer to</span>
             <span className="mr-3 likeDislike">
-              <img src="/img/like.png" width="25px" height="25px" alt="like" title="like feedback" />
-              <small>{item.counterLike}</small>
+              {/* eslint-disable-next-line max-len */}
+              {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions */}
+              <img
+                src="/img/like.png"
+                width="25px"
+                height="25px"
+                alt="like"
+                title="like feedback"
+                style={{ cursor: 'pointer' }}
+                onClick={this.likeFeedback}
+              />
+              <small>{item.likes.length}</small>
             </span>
             <span className="mr-3 likeDislike">
               <img src="/img/dislike.png" width="25px" height="25px" alt="dislike" title="dislike feedback" />
