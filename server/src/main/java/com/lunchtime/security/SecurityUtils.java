4e93012c79a55f5c017e197ea1f8bd3888c38548
Vitalii Makarchenkov <vitalii.mak@gmail.com>
2020-05-08 11:02:52 +0300

    Add method whic return person email

---
+++ server/src/main/java/com/lunchtime/security/SecurityUtils.java
@@ -0,0 +1,23 @@
+package com.lunchtime.security;
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public class SecurityUtils {
+    public static Optional<String> getCurrentPersonEmail() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(securityContext.getAuthentication())
+            .map(authentication -> {
+                if (authentication.getPrincipal() instanceof UserDetails) {
+                    UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+                    return springSecurityUser.getUsername();
+                } else if (authentication.getPrincipal() instanceof String) {
+                    return (String) authentication.getPrincipal();
+                }
+                return null;
+            });
+    }
+}
