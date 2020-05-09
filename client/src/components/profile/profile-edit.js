434e00b27abe20366bdbdafc3462d0cf5c6fa74d
Vitalii Makarchenkov <vitalii.mak@gmail.com>
2020-05-09 10:40:07 +0300

    Add input handlers

--- client/src/components/profile/profile-edit.js
+++ client/src/components/profile/profile-edit.js
@@ -12,17 +12,36 @@ class ProfileEdit extends Component {
     this.state = {
       // isShowAlert: false,
       user: this.props.user,
+      previousUserData: { ...this.props.user },
+
       // errors: {
       //   name: '', phone: '', password: '', err: 'Profile is not changed',
       // },
+      oldPassword: null,
+      newPassword: null,
+      confirmPassword: null,
     };
     // this.handleChange = this.handleChange.bind(this);
     // this.saveFormState = this.saveFormState.bind(this);
+    this.updateProfile = this.updateProfile.bind(this);
   }
 
-  updateProfile() {
-    console.log(this.state.user);
+  updateProfile(event) {
+    const form = event.currentTarget;
+    event.preventDefault();
+    event.stopPropagation();
+    console.log(this.state.user, this.state.previousUserData, form, this.state);
     this.props.history.push('/profile');
+  }
+
+  handleFormControl(event) {
+    this.state.user[event.target.name] = event.target.value;
+  }
+
+  handlePassword(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
 
   render() {
@@ -33,27 +52,64 @@ class ProfileEdit extends Component {
       <Container fluid>
         <Row className="profile-row">
           <Col md="6">
-            <Form>
+            <Form onSubmit={this.updateProfile}>
               <Form.Group controlId="formGroupName">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter a new name" />
+                <Form.Control
+                  type="text"
+                  name="name"
+                  placeholder="Enter a new name"
+                  defaultValue={user.name}
+                  onChange={(event) => this.handleFormControl(event)}
+                />
               </Form.Group>
               <Form.Group controlId="formGroupPhone">
                 <Form.Label>Phone number</Form.Label>
-                <Form.Control type="tel" placeholder="Enter a new phone number" />
+                <Form.Control
+                  type="tel"
+                  name="phoneNumber"
+                  placeholder="Enter a new phone number"
+                  defaultValue={user.phoneNumber}
+                  onChange={(event) => this.handleFormControl(event)}
+                />
               </Form.Group>
               <Form.Group controlId="formGroupOldPassword">
                 <Form.Label>Old password</Form.Label>
-                <Form.Control type="password" placeholder="Enter old password" />
+                <Form.Control
+                  type="password"
+                  name="oldPassword"
+                  placeholder="Enter old password"
+                  onChange={(event) => this.handlePassword(event)}
+                />
               </Form.Group>
               <Form.Group controlId="formGroupNewPassword">
                 <Form.Label>New password</Form.Label>
-                <Form.Control type="password" placeholder="Enter new password" />
+                <Form.Control
+                  type="password"
+                  name="newPassword"
+                  placeholder="Enter new password"
+                  onChange={(event) => this.handlePassword(event)}
+                />
               </Form.Group>
               <Form.Group controlId="formGroupConfirmPassword">
                 <Form.Label>Confirm new password</Form.Label>
-                <Form.Control type="password" placeholder="Confirm new password" />
+                <Form.Control
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirm new password"
+                  onChange={(event) => this.handlePassword(event)}
+                />
               </Form.Group>
+              <hr className="hr-border mt-0" />
+              <Link to="info">
+                <Button variant="danger" className="ml-3 mb-5">Cancel</Button>
+              </Link>
+              <Button
+                className="ml-3 mb-5"
+                type="submit"
+              >
+                Submit
+              </Button>
             </Form>
           </Col>
 
@@ -69,17 +125,7 @@ class ProfileEdit extends Component {
           </Col>
         </Row>
 
-        <hr className="hr-border mt-0" />
-        <Link to="info">
-          <Button variant="danger" className="ml-3 mb-5">Cancel</Button>
-        </Link>
-        <Button
-          className="ml-3 mb-5"
-          type="submit"
-          onClick={() => this.updateProfile()}
-        >
-          Submit
-        </Button>
+
       </Container>
     );
   }
