dede3c5521b99014cfa4f7453ea2d0a193e2fcc8
Voz4Nick <vmm408@gmail.com>
2020-05-11 13:08:19 +0300

    fix max file count to 20 item per one time

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const OWNER_ROLE_ID = 2;
-export const MAX_SELECTED_FILE = 10;
+export const MAX_SELECTED_FILE = 20;
 export const MAX_SELECTED_FILE_ERROR = 'Only 10 images can be uploaded at a time';
 export const WRONG_MIME_TYPE_ERROR = 'is not a supported format';
 export const MAX_FILE_SIZE = 5000000;
