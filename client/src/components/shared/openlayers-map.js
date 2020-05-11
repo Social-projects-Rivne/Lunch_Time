0f1f0d65d5d4e9a10171cef1e21188fc52822e49
Andrii Nazaruk <andriy_nazaruk@yahoo.de>
2020-05-11 08:25:18 -0700

    test

--- client/src/components/shared/openlayers-map.js
+++ client/src/components/shared/openlayers-map.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Map, layer, Layers } from 'react-openlayers';
+import {
+  Map, layer, Layers, custom,
+} from 'react-openlayers';
 import { Container } from 'react-bootstrap';
 import { transform } from 'ol/proj';
 import '../../styles/openlayers-map.css';
+import * as ol from 'openlayers';
+
+const iconFeature = new ol.Feature(new ol.geom.Point(transform([26.275728, 50.616294], 'EPSG:4326', 'EPSG:3857')));
+// const iconStyle = new ol.style.Style({
+// image: new ol.style.Icon({
+// anchor: [0.5, 46],
+// anchorXUnits: 'fraction',
+// anchorYUnits: 'pixels',
+// src: '/img/map-marker.png',
+// }),
+// });
+// iconFeature.setStyle(iconStyle);
+const source = new ol.source.Vector({ features: [iconFeature] });
+const marker = new custom.style.MarkerStyle('/img/map-pin-point.png');
 
 class OpenlayersMap extends Component {
+  addFeatures() {
+    const { restaurants } = this.props;
+    const points = restaurants.map((restaurant) => {
+      const point = {};
+      point[restaurant.id] = new ol.Feature(new ol.geom.Point(transform([restaurant.longitude,
+        restaurant.latitude], 'EPSG:4326', 'EPSG:3857')));
+      return point;
+    });
+    return points;
+  }
+
   render() {
     const { latitude, longitude } = this.props;
+
     return (
       <Container className="map-container">
         <Map view={{
@@ -20,7 +48,14 @@ class OpenlayersMap extends Component {
         >
           <Layers>
             <layer.Tile />
+            <layer.Vector source={source} style={marker.style} zIndex="1" />
           </Layers>
+          {/* <Overlays>
+<Overlay ref={comp => this.overlayComp = comp}>
+<custom.Popup ref={comp => this.popupComp = comp}>
+</custom.Popup>
+</Overlay>
+</Overlays> */}
         </Map>
       </Container>
     );
@@ -35,6 +70,7 @@ OpenlayersMap.defaultProps = {
 OpenlayersMap.propTypes = {
   latitude: PropTypes.string,
   longitude: PropTypes.string,
+  restaurants: PropTypes.any.isRequired,
 };
 
 export default OpenlayersMap;
